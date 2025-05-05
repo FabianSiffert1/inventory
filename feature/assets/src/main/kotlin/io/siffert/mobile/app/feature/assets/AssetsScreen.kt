341f6b42dcbbda7e66212d961f46785edcc423b2
FabianSiffert1 <siffert@sipgate.de>
2025-05-05 15:54:58 +0200

    ♻️(feature/assets): remove redundant preview

--- feature/assets/src/main/kotlin/io/siffert/mobile/app/feature/assets/AssetsScreen.kt
+++ feature/assets/src/main/kotlin/io/siffert/mobile/app/feature/assets/AssetsScreen.kt
@@ -1,16 +1,12 @@
 package io.siffert.mobile.app.feature.assets
 
-import androidx.compose.animation.Crossfade
 import androidx.compose.foundation.layout.Column
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.traceEventStart
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
-import io.siffert.mobile.app.inventory.core.designsystem.component.PreviewGradientBackgroundWrapper
-import io.siffert.mobile.app.inventory.core.designsystem.component.ThemePreviews
 import org.koin.androidx.compose.koinViewModel
 
 
@@ -20,8 +16,9 @@ internal fun AssetsScreen(modifier: Modifier = Modifier) {
     val uiState: AssetsScreenUiState by viewModel.uiState.collectAsStateWithLifecycle()
 
     Assets(
-       uiState = uiState,
-        modifier = modifier)
+        uiState = uiState,
+        modifier = modifier
+    )
 }
 
 @Composable
@@ -37,29 +34,4 @@ internal fun Assets(
             AssetsScreenUiState.Loading -> Text("LoadingStateImplPlaceholder")
             is AssetsScreenUiState.Success -> AssetList(assetList = uiState.assetList)
         }
-    }
-
-
-
-@ThemePreviews
-@Composable
-fun AssetsScreenPreview() =
-    PreviewGradientBackgroundWrapper {
-        val exampleAssetList = mutableListOf(
-            Asset(
-                name = "asset1", info = "currentValue and PnL", trend = Trend.UP
-            ),
-            Asset(
-                name = "asset2", info = "currentValue and PnL", trend = Trend.FLAT
-            ),
-            Asset(
-                name = "asset3", info = "currentValue and PnL", trend = Trend.DOWN
-            )
-        )
-        Assets(
-            uiState = AssetsScreenUiState.Success(
-                assetList = exampleAssetList
-            )
-        )
-
     }
