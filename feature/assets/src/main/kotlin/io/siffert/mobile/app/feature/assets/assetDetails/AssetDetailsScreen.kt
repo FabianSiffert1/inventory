80eb26c9de53cc0abd8b7dee8f1a2b0b02c3675a
FabianSiffert1 <siffert@sipgate.de>
2025-05-13 11:06:47 +0200

    ✨(feature/assets): Add empty, error, and loading states to AssetDetailsScreen

--- feature/assets/src/main/kotlin/io/siffert/mobile/app/feature/assets/assetDetails/AssetDetailsScreen.kt
+++ feature/assets/src/main/kotlin/io/siffert/mobile/app/feature/assets/assetDetails/AssetDetailsScreen.kt
@@ -1,14 +1,17 @@
 package io.siffert.mobile.app.feature.assets.io.siffert.mobile.app.feature.assets.assetDetails
 
 import androidx.compose.animation.Crossfade
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.statusBarsPadding
+import androidx.compose.material3.ListItem
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import org.koin.compose.viewmodel.koinViewModel
 import org.koin.core.parameter.parametersOf
@@ -32,16 +35,30 @@ private fun AssetDetailScreenContent(uiState: AssetDetailsScreenUiState, onBackC
         Crossfade(modifier = Modifier.padding(paddingValues), targetState = uiState) {
             when (it) {
                 AssetDetailsScreenUiState.Empty,
-                AssetDetailsScreenUiState.Error -> {
-                    Text(text = "Something went wrong")
-                }
-
+                AssetDetailsScreenUiState.Error,
                 AssetDetailsScreenUiState.Loading -> {
-                    Text(text = "Loading...")
+                    AssetDetailsEmptyStateListItem(state = it)
                 }
-
                 is AssetDetailsScreenUiState.Loaded -> AssetDetailsList(asset = it.asset)
             }
         }
     }
+}
+
+// todo: replace with proper loading state with animation
+@Composable
+private fun AssetDetailsEmptyStateListItem(state: AssetDetailsScreenUiState) {
+    ListItem(
+        modifier = Modifier.fillMaxWidth(),
+        headlineContent = {
+            val title =
+                when (state) {
+                    AssetDetailsScreenUiState.Empty -> "No asset data found"
+                    AssetDetailsScreenUiState.Error -> "Something went wrong"
+                    AssetDetailsScreenUiState.Loading -> "Loading..."
+                    is AssetDetailsScreenUiState.Loaded -> ""
+                }
+            Text(text = title, fontWeight = FontWeight.SemiBold)
+        },
+    )
 }
