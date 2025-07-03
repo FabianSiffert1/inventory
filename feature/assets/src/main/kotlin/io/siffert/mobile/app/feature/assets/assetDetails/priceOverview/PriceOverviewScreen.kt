2ccafa0c8e119b19853dfd07a641169707fe7eff
FabianSiffert1 <siffert@sipgate.de>
2025-07-03 16:53:27 +0200

    🚧 start working on PriceOverviewScreen - very very basic

---
+++ feature/assets/src/main/kotlin/io/siffert/mobile/app/feature/assets/assetDetails/priceOverview/PriceOverviewScreen.kt
@@ -0,0 +1,20 @@
+package io.siffert.mobile.app.feature.assets.io.siffert.mobile.app.feature.assets.assetDetails.priceOverview
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import io.siffert.mobile.app.inventory.core.designsystem.theme.InventoryTheme
+
+
+@Composable
+fun PriceOverviewScreen(modifier: Modifier = Modifier) {
+
+}
+
+@Composable
+@Preview
+private fun PriceOverviewScreenPreview(){
+    InventoryTheme {
+        PriceOverviewScreen()
+    }
+}
